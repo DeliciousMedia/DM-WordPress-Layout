ec2567b95fdc4b7fb039bc72b110d4cb995223a5
Dave Pullig <dave@deliciousmedia.co.uk>
2017-09-13 15:42:34 +0100

    Tidying up code & docs to meet WordPress Coding Standards

--- content/mu-plugins/dm-loader.php
+++ content/mu-plugins/dm-loader.php
@@ -1,14 +1,17 @@
 <?php
-/*
-Plugin Name: DM Must Use Plugin Loader
-Description: Loads MU plugins from subfolders.
-Author: Delicious Media Limited
-Author URI: https://www.deliciousmedia.co.uk/
-Version: 1.1
-*/
+/**
+ * Plugin Name: DM Must Use Plugin Loader
+ * Description: Loads MU plugins from subfolders.
+ * Author: Delicious Media Limited
+ * Author URI: https://www.deliciousmedia.co.uk/
+ * Version: 1.1
+ *
+ * @package  DM-WordPress-layout
+ */
 
-if ( ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) )
+if ( ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) ) {
 	return;
+}
 
 $dm_plugins = array(
 	'dm/dm-environment-indicators.php',
@@ -19,46 +22,53 @@ foreach ( $dm_plugins as $dm_plugin ) {
 	require_once WPMU_PLUGIN_DIR . '/' . $dm_plugin;
 }
 
-add_action( 'pre_current_active_plugins', function () use ( $dm_plugins ) {
-	global $plugins, $wp_list_table;
-	// Add our own mu-plugins to the page
-	foreach ( $dm_plugins as $plugin_file ) {
-		$plugin_data = get_plugin_data( WPMU_PLUGIN_DIR . "/$plugin_file", false, false ); //Do not apply markup/translate as it'll be cached.
-		if ( empty ( $plugin_data['Name'] ) )
-			$plugin_data['Name'] = $plugin_file;
-		$plugins['mustuse'][ $plugin_file ] = $plugin_data;
+add_action(
+	'pre_current_active_plugins', function () use ( $dm_plugins ) {
+		global $plugins, $wp_list_table;
+		// Add our own mu-plugins to the page.
+		foreach ( $dm_plugins as $plugin_file ) {
+			$plugin_data = get_plugin_data( WPMU_PLUGIN_DIR . "/$plugin_file", false, false ); // Do not apply markup/translate as it'll be cached.
+			if ( empty( $plugin_data['Name'] ) ) {
+				$plugin_data['Name'] = $plugin_file;
+			}
+			$plugins['mustuse'][ $plugin_file ] = $plugin_data;
+		}
+		// Recount totals.
+		$GLOBALS['totals']['mustuse'] = count( $plugins['mustuse'] );
+		// Only apply the rest if we're actually looking at the page.
+		if ( 'mustuse' !== $GLOBALS['status'] ) {
+			return;
+		}
+		// Reset the list table's data.
+		$wp_list_table->items = $plugins['mustuse'];
+		foreach ( $wp_list_table->items as $plugin_file => $plugin_data ) {
+			$wp_list_table->items[ $plugin_file ] = _get_plugin_data_markup_translate( $plugin_file, $plugin_data, false, true );
+		}
+		$total_this_page = $GLOBALS['totals']['mustuse'];
+		if ( $GLOBALS['orderby'] ) {
+			uasort( $wp_list_table->items, array( $wp_list_table, '_order_callback' ) );
+		}
+		// Remove register-theme-directory.php from the list.
+		// Force showing all plugins.
+		// See https://core.trac.wordpress.org/ticket/27110.
+		$plugins_per_page = $total_this_page;
+		$wp_list_table->set_pagination_args(
+			array(
+				'total_items' => $total_this_page,
+				'per_page' => $plugins_per_page,
+			)
+		);
 	}
-	// Recount totals
-	$GLOBALS['totals']['mustuse'] = count( $plugins['mustuse'] );
-	// Only apply the rest if we're actually looking at the page
-	if ( $GLOBALS['status'] !== 'mustuse' )
-		return;
-	// Reset the list table's data
-	$wp_list_table->items = $plugins['mustuse'];
-	foreach ( $wp_list_table->items as $plugin_file => $plugin_data ) {
-		$wp_list_table->items[$plugin_file] = _get_plugin_data_markup_translate( $plugin_file, $plugin_data, false, true );
-	}
-	$total_this_page = $GLOBALS['totals']['mustuse'];
-	if ( $GLOBALS['orderby'] ) {
-		uasort( $wp_list_table->items, array( $wp_list_table, '_order_callback' ) );
-	}
-	// Remove register-theme-directory.php from the list.
+);
 
-	// Force showing all plugins
-	// See https://core.trac.wordpress.org/ticket/27110
-	$plugins_per_page = $total_this_page;
-	$wp_list_table->set_pagination_args( array(
-		'total_items' => $total_this_page,
-		'per_page' => $plugins_per_page,
-	) );
-});
+add_action(
+	'network_admin_plugin_action_links', function ( $actions, $plugin_file, $plugin_data, $context ) use ( $dm_plugins ) {
+		if ( 'mustuse' !== $context || ! in_array( $plugin_file, $dm_plugins ) ) {
+			return;
+		}
+		$actions[] = sprintf( '<span style="color:#333">File: <code>%s</code></span>', $plugin_file );
+		return $actions;
+	}, 10, 4
+);
 
-add_action( 'network_admin_plugin_action_links', function ( $actions, $plugin_file, $plugin_data, $context ) use ( $dm_plugins ) {
-	if ( $context !== 'mustuse' || ! in_array( $plugin_file, $dm_plugins ) ) {
-		return;
-	}
-	$actions[] = sprintf( '<span style="color:#333">File: <code>%s</code></span>', $plugin_file );
-	return $actions;
-}, 10, 4 );
-
-unset($dm_plugins);
+unset( $dm_plugins );
--- wp-config.php
+++ wp-config.php
@@ -6,13 +6,18 @@
 /**
  * Always load the database credentials & salts from a seperate file.
  */
-if ( ! file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) die( 'Local config file is missing.' );
+if ( ! file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+	die( 'Local config file is missing.' );
+}
 include( dirname( __FILE__ ) . '/local-config.php' );
 
 /**
  * Setup URLS and the custom content directory.
  */
-$dm_protocol = ( ! empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] != 'off' ) ? 'https://' : 'http://';
+if ( ! isset( $_SERVER['HTTP_HOST'] ) ) {
+	$_SERVER['HTTP_HOST'] = ''; // Prevent wp-cli complaining about missing host.
+}
+$dm_protocol = ( ! empty( $_SERVER['HTTPS'] ) && 'off' != $_SERVER['HTTPS'] ) ? 'https://' : 'http://';
 define( 'WP_SITEURL', $dm_protocol . $_SERVER['SERVER_NAME'] . '/wp' );
 define( 'WP_HOME', $dm_protocol . $_SERVER['SERVER_NAME'] );
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
@@ -28,7 +33,7 @@ define( 'WPLANG', '' );
 /**
  * Set some defaults, which we may override in local-config.php
  */
-defined( 'WP_POST_REVISIONS' ) || define( 'WP_POST_REVISIONS', 50 ) ;
+defined( 'WP_POST_REVISIONS' ) || define( 'WP_POST_REVISIONS', 50 );
 
 /**
  * If we're in a development environment, enable debugging.
@@ -47,12 +52,14 @@ if ( 'DEV' === DM_ENVIRONMENT ) {
 /**
  * Load memcached settings, if they are there.
  */
-if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
+if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) ) {
 	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
+}
 
 /**
  * Load WordPress.
  */
-if ( ! defined( 'ABSPATH' ) )
+if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+}
 require_once( ABSPATH . 'wp-settings.php' );
